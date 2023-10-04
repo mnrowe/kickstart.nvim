65a886f354b77e91ea89b4cb5b40e6c94b93c920
Matthew Rowe <mrowe.w@gmail.com>
2023-10-04 14:17:27 -0400

    disable indent line bugged

--- init.lua
+++ init.lua
@@ -128,11 +128,11 @@ require('lazy').setup({
 
   {
     -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
+    --'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    main = "ibl",
-    opts = {},
+    --main = "ibl",
+    --opts = {},
   },
 
   -- "gc" to comment visual regions/lines
