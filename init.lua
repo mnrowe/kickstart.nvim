95be225ceccb7fac85b437a97eab9435b210df4c
rwe <mrowe.w@gmail.com>
2023-10-19 16:27:55 -0400

    add indent lines back

--- init.lua
+++ init.lua
@@ -128,7 +128,7 @@ require('lazy').setup({
 
   {
     -- Add indentation guides even on blank lines
-    --'lukas-reineke/indent-blankline.nvim',
+    'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     --main = "ibl",
