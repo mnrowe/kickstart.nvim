efc413c5179d6589c1d4159674d367df9feab501
rwe <mrowe.w@gmail.com>
2024-04-18 12:55:06 -0400

    update

--- init.lua
+++ init.lua
@@ -131,8 +131,8 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    --main = "ibl",
-    --opts = {},
+    main = "ibl",
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
